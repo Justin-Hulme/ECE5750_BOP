7c8f12482c05989a1ec2e19be09639b03afa6f35
Cole <coleshanefrancis@gmail.com>
2026-02-09 18:21:11 -0700

    Refactored scoreboard to handle offset idx rather than offset. This means we dont have to use a map but instead can index a static const array

--- prefetcher/BOP/offset_scoreboard.h
+++ prefetcher/BOP/offset_scoreboard.h
@@ -8,14 +8,6 @@
 #define ROUNDMAX 100
 #define BADSCORE 1
 
-uint64_t POTENTIAL_OFFSETS[] = {1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 
-                                    15, 16, 18, 20, 24, 25, 27, 3, 
-                                    32, 36, 40, 45, 48, 50, 54, 60, 
-                                    64, 72, 75, 80, 81, 90, 96, 100, 
-                                    108, 120, 125};
-
-uint64_t NUM_OFFSETS = sizeof(POTENTIAL_OFFSETS) / sizeof(POTENTIAL_OFFSETS[0]);
-
 class OffsetScoreboard{
     public:
         OffsetScoreboard();
@@ -23,54 +15,59 @@ class OffsetScoreboard{
         void reset_scores();
         uint64_t get_best_offset();
 
+        static const uint64_t POTENTIAL_OFFSETS[] = {1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 
+                                                15, 16, 18, 20, 24, 25, 27, 3, 
+                                                32, 36, 40, 45, 48, 50, 54, 60, 
+                                                64, 72, 75, 80, 81, 90, 96, 100, 
+                                                108, 120, 125};
+
+        static constexpr uint16_t NUM_OFFSETS = sizeof(potential_offsets) / sizeof(potential_offsets[0]);
+
     private:
-        std::map<uint64_t, uint16_t> m_scores;
+        uint64_t m_scores[NUM_OFFSETS];
 };
 
 OffsetScoreboard::OffsetScoreboard() 
 {
-    for (uint64_t offset : POTENTIAL_OFFSETS) 
+    for (uint64_t &score : m_scores) 
     {
-        m_scores[offset] = 0;
+        score = 0;
     }
 }
 
-bool OffsetScoreboard::inc_score(uint64_t offset) 
+bool OffsetScoreboard::inc_score(uint64_t offset_idx) 
 /**
  * increment offset score
  * return true if the score  reaches scoremax else return false
  */
 {
-    m_scores[offset]++;
+    m_scores[offset_idx]++;
 
-    return (m_scores[offset] == SCOREMAX);
+    return (m_scores[offset_idx] >= SCOREMAX);
 }
 
 void OffsetScoreboard::reset_scores() 
-/**
- * Reset all scores to 0
- */
 {
-    for (uint64_t offset : POTENTIAL_OFFSETS) 
+    for (uint64_t &score : m_scores) 
     {
-        m_scores[offset] = 0;
+        score = 0;
     }
 }
 
 uint64_t OffsetScoreboard::get_best_offset()
 /**
- * Returns the score with the highest score;
+ * Returns the offset with the highest score;
  */
 {
     uint64_t best_offset = 1;
     uint16_t best_score = 0;
 
-    for(const auto& [offset, score] : m_scores)
+    for (int ix = 0; idx < NUM_OFFSETS; idx++)
     {
-        if (score > best_score)
+        if (scores[idx] > best_score) 
         {
-            best_offset = offset;
-            best_score = score;
+            best_score = scores[idx];
+            best_offset = potential_offsets[idx];
         }
     }
 
